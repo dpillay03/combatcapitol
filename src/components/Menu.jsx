e942488a51405abd669ce295f3f4ff09a00faf75
dpillay03 <dpillay03@gmail.com>
2020-11-03 10:33:50 -1000

    ready for deployment

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,66 +15,98 @@ import Logo from '../assets/combat_capitol_logo.png'
 import '../style/menu.css'
 
 export default class Menu extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            visibility: false
-        };
-        // change code below this line
-        this.toggleVisibility = this.toggleVisibility.bind(this)
-        // change code above this line
+            showMenu: false,
+        }
+        this.showMenu = this.showMenu.bind(this);
     }
-    // change code below this line
-    toggleVisibility() {
-        if (this.state.visibility = true) {
-            this.setState = ({
-                visibility: false
-            });
-        }
-        else {
-            this.setState = ({
-                visibility: true
-            })
-        }
+    showMenu(event) {
+        event.preventDefault();
+        this.setState({
+            showMenu: !this.state.showMenu
+        });
     }
 
 
     render() {
         return <React.Fragment>
             <Router>
-                
                 <div>
                     <div class="menu-container">
                         <a href="https://combatcapital.com" ><img src={Logo} id="logo" alt="the combat capital logo" /></a>
                     </div>
-                    <div class="menu">
-                        <nav>
-                            <ul>
-                                <li>
+                    <div className="main-menu">
+                        <nav className="main-menu-nav">
+                            <ul className="main-menu-ul">
+                                <li className="main-menu-li">
                                     <Link to="/">Home</Link>
                                 </li>
-                                <li>
+                                <li className="main-menu-li">
                                     <Link to="/mma">MMA</Link>
                                 </li>
-                                <li>
+                                <li className="main-menu-li">
                                     <Link to="/jiu-jitsu">Jiu Jitsu</Link>
                                 </li>
-                                <li>
+                                <li className="main-menu-li">
                                     <Link to="/boxing">Boxing</Link>
                                 </li>
-                                <li>
+                                <li className="main-menu-li">
                                     <Link to="/wrestling">Wrestling</Link>
                                 </li>
-                                <li>
+                                <li className="main-menu-li">
                                     <Link to="/muay-thai">Muay Thai</Link>
                                 </li>
-                                <li>
+                                <li className="main-menu-li">
                                     <Link to="/judo">Judo</Link>
                                 </li>
                             </ul>
                         </nav>
+                        <div id="hamburger"
+                        onClick={this.showMenu}
+                        tabindex="1">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    {
+                        this.state.showMenu
+                            ? (
+                                <nav id="mobile-menu-nav">
+                                    <ul id="mobile-ul-nav">
+                                        <li className="mobile-menu-li">
+                                            <Link to="/">Home</Link>
+                                        </li>
+                                        <li className="mobile-menu-li">
+                                            <Link to="/mma">Mixed Martial Arts</Link>
+                                        </li>
+                                        <li className="mobile-menu-li">
+                                            <Link to="/jiu-jitsu">Jiu Jitsu</Link>
+                                        </li>
+                                        <li className="mobile-menu-li">
+                                            <Link to="/boxing">Boxing</Link>
+                                        </li>
+                                        <li className="mobile-menu-li">
+                                            <Link to="/wrestling">Wrestling</Link>
+                                        </li>
+                                        <li className="mobile-menu-li">
+                                            <Link to="/muay-thai">Muay Thai</Link>
+                                        </li>
+                                        <li className="mobile-menu-li">
+                                            <Link to="/judo">Judo</Link>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            )
+                            : (
+                                null
+                            )
+                    }
                     </div>
                     <div class="clearfix"></div>
+                    <div>
+                </div>
 
 
                     <Route exact path="/" component={Home} />

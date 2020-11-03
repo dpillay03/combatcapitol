245f5d3a18610ed6b190e6bd5f3275e6abaef726
dpillay03 <dpillay03@gmail.com>
2020-11-03 10:35:43 -1000

    Updates

---
+++ precache-manifest.58202851ec13f176fe46ef1137481a33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a24f9189ed63cd3d660f2991300c839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fc60b16734cf35ac56d",
+    "url": "/static/css/main.7915a580.chunk.css"
+  },
+  {
+    "revision": "73a3c0e25b4c38304db1",
+    "url": "/static/js/2.bb5616f6.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/static/js/2.bb5616f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc60b16734cf35ac56d",
+    "url": "/static/js/main.dd13af22.chunk.js"
+  },
+  {
+    "revision": "fdb10fb2ef5deb3ab78d",
+    "url": "/static/js/runtime-main.cd40c800.js"
+  },
+  {
+    "revision": "60912907870a6135bbaa0a67fd119ff9",
+    "url": "/static/media/combat_capitol_logo.60912907.png"
+  }
+]);
